e2d2cb185bd79e5e2b8babc0d6d15d43bd40e94b
Aleksefo <aleksefo@gmail.com>
2016-11-16 14:02:23 +0200

    Moved Navigation Component to separate file

---
+++ components/navigation-component/NavigationComponent.js
@@ -0,0 +1,148 @@
+import Exponent from 'exponent';
+import React, {Component} from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableHighlight,
+	NavigationExperimental,
+	ScrollView
+} from 'react-native';
+
+import { Components } from 'exponent';
+import MapViewComponent from '../map-component/MapViewComponent';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			navState: NavReducer(undefined, {})
+		}
+	}
+	_handleAction (action) {
+		const newState = NavReducer(this.state.navState, action);
+		if (newState === this.state.navState) {
+			return false;
+		}
+		this.setState({
+			navState: newState
+		})
+		return true;
+	}
+	handleBackAction() {
+		return this._handleAction({ type: 'pop' });
+	}
+	_renderRoute (key) {
+		if (key === 'Page1') {
+			return <Page1
+				onPress={this._handleAction.bind(this,
+					{ type: 'push', key: 'Page2' })} />
+		}
+		if (key === 'Page2') {
+			return <Page2
+				goBack={this.handleBackAction.bind(this)}
+				onPress={this._handleAction.bind(this,
+					{ type: 'push', key: 'Page3' })} />
+		}
+		if (key === 'Page3') {
+			return <Page3
+				goBack={ this.handleBackAction.bind(this)} />
+		}
+	}
+	_renderScene(props) {
+		const ComponentToRender = this._renderRoute(props.scene.route.key)
+		return (
+			<ScrollView style={styles.scrollView}>
+				{ComponentToRender}
+			</ScrollView>
+		);
+	}
+
+
+	render() {
+		return (
+			<NavigationCardStack
+				navigationState={this.state.navState}
+				onNavigate={this._handleAction.bind(this)}
+				renderScene={this._renderScene.bind(this)}
+			/>
+		)
+	}
+}
+
+const {
+	CardStack: NavigationCardStack,
+	StateUtils: NavigationStateUtils
+} = NavigationExperimental
+function createReducer(initialState) {
+	return (currentState = initialState, action) => {
+		switch (action.type) {
+			case 'push':
+				return NavigationStateUtils.push(currentState, {key: action.key});
+			case 'pop':
+				return currentState.index > 0 ?
+					NavigationStateUtils.pop(currentState) :
+					currentState;
+			default:
+				return currentState;
+		}
+	}
+}
+const NavReducer = createReducer({
+	index: 0,
+	key: 'App',
+	routes: [{key: 'Page1'}]
+})
+const Button = ({title, onPress}) => (
+	<TouchableHighlight
+		underlayColor='#EFEFEF'
+		onPress={onPress}
+		style={styles.button}>
+		<Text>{title}</Text>
+	</TouchableHighlight>
+)
+const Page1 = ({ onPress }) => (
+	<View style={styles.container}>
+		<Button onPress={onPress} title='Next' />
+	</View>
+)
+const Page2 = ({ onPress, goBack }) => (
+	<View style={styles.container}>
+		<Button onPress={onPress} title='Next' />
+		<Button onPress={goBack} title='Back' />
+	</View>
+)
+const Page3 = ({ goBack }) => (
+	<View style={styles.container}>
+		<Button title='Back' onPress={goBack} />
+		<MapViewComponent/>
+	</View>
+
+
+)
+
+
+const styles = StyleSheet.create({
+	scrollView: {
+		backgroundColor: '#D19FFF',
+		flex: 1
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF497'
+	},
+	title: {
+		fontSize: 30,
+		marginTop: 30,
+		textAlign: 'center'
+	},
+	button: {
+		height: 40,
+		marginTop: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10,
+		backgroundColor: '#EDEDED'
+	}
+})
