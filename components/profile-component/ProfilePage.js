7d4c9ac714fe46ac90ac7e2f31bea3b6096ffa36
mikaelgo <iosdev@ety-p-mbp-02.local>
2016-12-12 13:29:44 +0200

    profile page placeholders

--- components/profile-component/ProfilePage.js
+++ components/profile-component/ProfilePage.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import Button from 'react-native-button';
 import styles from '../../resources/styles.js';
+import api from '../utils/APImanager.js'
 
 import  {
   StyleSheet,
@@ -26,13 +27,28 @@ export default class ProfilePage extends Component {
     },
   }
 
+  componentWillMount(){
+
+  }
+
+  componentDidMount(){
+
+    api.getSome("user").then(response => {
+      console.log("getSome callback category " , response);
+    });
+
+  }
+
   render() {
     return (
       <View style={{flex:1}}>
           <View style={{flex: 1}}>
                 <View style={styles.profileInfoStyle}>
+                  <View style={{width:50, height:50, backgroundColor:'red', marginTop:10, marginLeft: 10, borderRadius:30}}></View>
                   <View style={styles.profileInfoTextStyle}>
-                  <Text>Profile Info</Text>
+                  <Text style={{fontSize:35, color: 'white', marginLeft: 10}}>User Name</Text>
+                  <Text style={{marginLeft:10, marginTop:2, color:'white'}}>Profile Info</Text>
+                  <Text style={{textAlign: 'right', fontSize:20, marginBottom: 2, marginRight:5, color:'white' }}>age, gender</Text>
                   </View>
                 </View>
 
@@ -81,4 +97,5 @@ export default class ProfilePage extends Component {
   deleteAccount() {
     console.log("<------------DELETE ACCOUNT PUSHED------------->");
   }
+
 }
